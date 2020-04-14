25dd89664d88a5d672cdf99303b6b14813fbe8e6
hank amadeus <hankamadeus@email.com>
2020-04-14 16:32:45 -0400

    typed logical steps and actions using comments on js file to setup how the application will work and operate with user...also changed id names for textarea and generate button

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+// STEP AND PROCESS TO GET APPLICATION TO OPERATE/FUNCTION
+
+// On click of Generate Password button
+
+// 1. Prompt user "How many characters would you like your password to contain" (A number => 8 <= 128 or false)
+
+// 2. Confirm with user to include the use of special characters (if true include use of these special characters at random !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~) or if false do not include)
+
+// 3. Confirm with user to include use of numeric characters (if true include use of these numbers at random 0123456789 or if false do not include)
+
+// 3. Confirm with user to include the use of lowercase letters (if true include use of lowercase letters at random abcdefghijklmnopqrstuvwxyz or if false do not include)
+
+// 4. Confirm with user to include the use of uppercase letters (if true include use of uppercase letters at random ABCDEFGHIJKLMNOPQRSTUVWXYZ or if false do not include)
+
+//----> Finished customized random password is then displayed in textarea 
+
+// HTML ELEMENTS TO BE MANIPULATED BY JS:  #password-section, #generate-button
