8f359ae7802764032ceac44f4a46d8c445505eba
hank amadeus <hankamadeus@email.com>
2020-04-17 18:45:58 -0400

    rewrote majority of script due to variable accessibilty and ease of access, user inputs now stored into an array rather than object, character types are now arrays rather than strings

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,59 +17,122 @@
 // HTML ELEMENTS TO BE MANIPULATED BY JS:  #password-section, #generate-button
 
 // DOM VARIABLES
-var passwordSection_TextArea = document.querySelector("#password-section");
-var generatePassword_Button = document.querySelector("#generate-button");
+var displayPasswordSection_ = document.querySelector("#password-section");
+var generatePasswordButton = document.querySelector("#generate-button");
 
-// Defined variables for password options/characters
-var specialChar = "~!@#$%^&*(){};:<>/?=+";
-var lowerChar = "abcdefghijklmnopqrstuvwxyz";
-var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numberChar = "0123456789";
+// Arrays containing password types/characters
+var specialChar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", ";", ":", "<", ">", "/", "?", "=", "+"];
+var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-// Empty object that will stores users desired types of characters to be used in password
-var userChoices = {};
+// Defined deafault variables before user input
+var userCharLength = 0;
+var useSpecialChar = false;
+var useLowerChar = false;
+var useUpperChar = false;
+var useNumberChar = false;
+
+// Empty ARRAY that will stores users desired types of characters to be used in password
+var userChoiceArray = [];
 
 // Event on-click function to prompt user password length, ensures length is at least 8 characters and no more than 128 characters, if password length meets requirements, runs another function called confirmCharacters which is detailed below
 
-generatePassword_Button.addEventListener("click", function () {
+generatePasswordButton.addEventListener("click", function () {
     event.preventDefault();
-    var promptPasswordLength = parseInt(prompt("How many characters would you like your password to contain? (must be between 8 and 128 characters)"));
-    if (promptPasswordLength >= 8 && promptPasswordLength <= 128) {
-        confirmCharacters();
-    } else if (promptPasswordLength <= 8 || promptPasswordLength >= 128) {
-        alert("Note: Password must be at least 8 characters and no longer than 128 characters");
-    }
+    userInputs();
+    lengthCheck();
+    generateUserChoiceArray();
+
 });
 
-// Function that confirms with user what characters they would like to include in their password and stores character into empty object
-function confirmCharacters() {
-    var confirmSpecialChar = confirm("Would you like to include special characters? (~!@#$%^&*(){};:<>/?=+)");
-    var confirmLowerChar = confirm("Would you like to include lower case letters?");
-    var confirmUpperChar = confirm("Would you like to include upper case letters?");
-    var confirmNumberChar = confirm("Would you like to include numbers?");
+function userInputs() {
+    userCharLength = parseInt(prompt("How many characters would you like your password to contain? (must be between 8 and 128 characters)"));
+    useSpecialChar = confirm("Would you like to include special characters? (~!@#$%^&*(){};:<>/?=+)");
+    useLowerChar = confirm("Would you like to include lower case letters?");
+    useUpperChar = confirm("Would you like to include upper case letters?");
+    useNumberChar = confirm("Would you like to include numbers?");
 
-    // If statements that take users answers to confirms and add to the empty userChoice object
-    if (confirmSpecialChar === true) {
-        userChoices["specialChar"] = specialChar;
+    // console.log(useSpecialChar);
+    // console.log(useLowerChar);
+    // console.log(userCharLength);
+    // console.log(useUpperChar);
+    // console.log(useNumberChar);
+}
+
+function lengthCheck() {
+    if (userCharLength >= 8 && userCharLength <= 128) {
+    } else if (userCharLength <= 8 || userCharLength >= 128) {
+        alert("Note: Password must be at least 8 characters and no longer than 128 characters, please refresh and try again");
     }
-    if (confirmLowerChar === true) {
-        userChoices["lowerChar"] = lowerChar;
+}
+
+function generateUserChoiceArray() {
+    if (useSpecialChar === true) {
+        userChoiceArray = userChoiceArray.concat(specialChar);
     }
-    if (confirmUpperChar === true) {
-        userChoices["upperChar"] = upperChar;
+    if (useLowerChar === true) {
+        userChoiceArray = userChoiceArray.concat(lowerChar);
     }
-    if (confirmNumberChar === true) {
-        userChoices["numberChar"] = numberChar;
+    if (useUpperChar === true) {
+        userChoiceArray = userChoiceArray.concat(upperChar);
     }
-    console.log(userChoices);
-};
+    if (useNumberChar === true) {
+        userChoiceArray = userChoiceArray.concat(numberChar);
+    }
+
+
+}
 
 
 
-    // console.log(promptPasswordLength);
-    // console.log(confirmSpecialChar);
-    // console.log(confirmLowerChar);
-    // console.log(confirmUpperChar);
-    // console.log(confirmNumberChar);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function producedPassword() {
+//     for (var i = 0; i < userCharLength; i++) {
+//         var passwordString = "";
+//         var passwordChars = Object.keys(userChoices);
+//         var randomizedEl = passwordChars[Math.floor(Math.random() * passwordChars.length)]
+//         var singleChar = singleRandomChar(userChoices[passwordChars]);
+
+//         passwordString += singleChar;
+
+//         console.log("single random char: ", singleChar);
+//         console.log(passwordString, passwordString.length);
+//         console.log(userCharLength);
+
+
+//     }
+//     return passwordString
+// }
+
+// function singleRandomChar(takesString) {
+//     return takesString[Math.floor(Math.random() * takesString.length)]
+// }
+
+// producedPassword();
+
+
 
 
